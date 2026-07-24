a3302fa10a56cfa87f428d208472125ca97f49c3
Raghunandan Saini <raghunandansaini27@gmail.com>
2026-07-24 10:25:57 +0530

    fixed cors issue,implemented dom,etc

--- projects/api/main.py
+++ projects/api/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.services import smart_search,get_ranked_jobs_by_location_and_keyword_hybrid_score,initialize_vectors,refresh_vectors
 
 app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def startup_event():
--- projects/frontend/script.js
+++ projects/frontend/script.js
@@ -1,1 +1,112 @@
 console.log("Javascript Connected Successfully!");
+
+const button = document.getElementById("search-btn");
+
+const searchBox = document.getElementById("search-box");
+
+const results=document.getElementById("results");
+
+button.addEventListener("click",function()
+{
+	console.log(searchBox.value);
+	searchJobs();
+});
+
+searchBox.addEventListener("keydown",function(event)
+{
+	if(event.key==="Enter")
+	{
+		event.preventDefault();
+		searchJobs();
+	}
+});
+
+async function searchJobs() 
+{
+	const query=searchBox.value;
+
+	const url=`http://127.0.0.1:8000/jobs/smart-search?query=${query}`;
+
+	showMessage("Searching...");
+	
+	const response=await fetch(url);
+
+	const jobs= await response.json()
+
+	showMessage("")
+
+	if(jobs.length===0)
+	{
+		showMessage("No Results found! Try another keyword.")
+	}
+
+	else{
+	
+	for (const job of jobs)
+	{
+		const jobCard=createJobCard(job);
+
+		results.appendChild(jobCard);
+	}
+
+	console.log(jobs)
+	
+	console.log(results)
+}
+}
+
+function showMessage(message)
+{
+	results.innerHTML=`<p class=message>${message}<p>`;
+}
+
+function createJobCard(job)
+{
+	const card=document.createElement("div");
+
+	card.className="job-card";
+
+	const subcard=document.createElement("div");
+
+	subcard.className="job-header";
+
+	const job_title=document.createElement("h3");
+
+	const apply_button=document.createElement("button");
+
+	apply_button.addEventListener("click",function()
+		{
+			window.open(job.link, "_blank")
+		}
+	)
+
+	const job_company=document.createElement("p");
+
+	const job_location=document.createElement("p");
+
+	const job_description=document.createElement("p");
+
+	job_title.textContent=job.title;
+
+	apply_button.textContent="Apply";
+
+	job_company.textContent=job.company;
+
+	job_location.textContent=job.location;
+
+	job_description.textContent=job.description;
+
+	subcard.appendChild(job_title);
+
+	subcard.appendChild(apply_button);
+
+	card.appendChild(subcard);
+
+	card.appendChild(job_company);
+
+	card.appendChild(job_location);
+
+	card.appendChild(job_description);
+
+	return card;
+}
